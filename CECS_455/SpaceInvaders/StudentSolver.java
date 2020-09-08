a534bc9fe536647064fc7c9e07992fb2ab4aaf89
Donovan Lee <32886721+LeeDonovan@users.noreply.github.com>
2020-09-08 10:45:01 -0700

    Update 9/8

--- CECS_455/SpaceInvaders/StudentSolver.java
+++ CECS_455/SpaceInvaders/StudentSolver.java
@@ -1,14 +1,27 @@
 import java.util.*;
+import java.lang.Math;
 
 public class StudentSolver{
 
     public static HashSet<Integer> solve(HashMap<Integer, Integer> aliens){
-        int counter = 0;
-        for(int i = 0; i < aliens.size(); i++){
-            counter+=aliens.get(i);
-        }
+        double sum = 0;
+        int n = aliens.size();
+        int increase = 1;
         HashSet<Integer> all = new HashSet<Integer>();
-        all.add(counter);
+        if( n > 0 ){
+            for(int i = 0; i < n; i++){
+                
+                sum += (Math.pow(2,-aliens.get(increase)));//summation formula
+                System.out.println(sum);
+                if(increase != n ){
+                    increase++;
+                }
+                
+            }
+        }
+        
+        System.out.println(sum);
+        
         return all;
     }
 
@@ -19,13 +32,21 @@ public class StudentSolver{
         HashMap<Integer,Integer> aliens = new HashMap<Integer,Integer>();
         System.out.println("Please enter a number: ");
         int counter = input.nextInt(); //gets the number of aliens you want
-        while(counter > 0){
-            aliens.put(counter, counter); //adds in number of aliens into hashmap
+        int hold = counter;
+        System.out.println("Please enter a Levels: ");
+        int level = input.nextInt();//gets the number of levels
+        while(counter > 0 ){
+            aliens.put(counter,level);
             counter--;
+            if(counter % 5 == 0){
+                level--;
+            }
         }
-        HashSet<Integer> total = new HashSet<Integer>();
-        total = solve(aliens);
-        System.out.println(total);
+        System.out.println(aliens + " " + aliens.size());
+        HashSet<Integer> totalAliens = new HashSet<Integer>();
+        totalAliens = solve(aliens);
+        System.out.println(totalAliens);
+        
 
         
     }
--- Test/Java/test.java
+++ Test/Java/test.java
@@ -1,6 +1,10 @@
+import java.lang.Math;
+
 public class test {
 
     public static void main(String args[]){
-        System.out.println(7%13);
+        int first = ((int)Math.pow(2,3)) * ((int)Math.pow(2,4) + 4);
+        System.out.println(first);
+        
     }
 }
