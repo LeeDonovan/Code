d65b9b0b6018f15fa2a0fd4f04bd94afd4a83a3d
Donovan Lee <32886721+LeeDonovan@users.noreply.github.com>
2020-09-09 23:49:00 -0700

    Test

--- CECS_455/SpaceInvaders/StudentSolver.java
+++ CECS_455/SpaceInvaders/StudentSolver.java
@@ -1,26 +1,41 @@
+/*
+Donovan Lee
+CECS_455
+Lab 1: Space Invaders
+9/10/2020
+*/
+
 import java.util.*;
-import java.lang.Math;
+
 
 public class StudentSolver{
 
     public static HashSet<Integer> solve(HashMap<Integer, Integer> aliens){
         double sum = 0;
-        int n = aliens.size();
-        int increase = 1;
+        int magicNum = 1 ; // ? 
+        int counter = 0;
+        int adder = 0;
+        Iterator traverse = aliens.entrySet().iterator();// go thru Hashmap
         HashSet<Integer> all = new HashSet<Integer>();
-        if( n > 0 ){
-            for(int i = 0; i < n; i++){
-                
-                sum += (Math.pow(2,-aliens.get(increase)));//summation formula
-                System.out.println(sum);
-                if(increase != n ){
-                    increase++;
-                }
-                
+        while(traverse.hasNext()){
+            counter++;
+            Map.Entry alienSet = (Map.Entry)traverse.next();// goes thru hashmap
+            System.out.println(alienSet);
+            int alienID = (int)alienSet.getKey();
+            System.out.println("Alien ID: " + alienID);
+            sum += (Math.pow(2,-aliens.get(alienID)));//get alien's level from beginning
+            if(sum >= magicNum){
+                break;
             }
         }
-        
-        System.out.println(sum);
+
+        System.out.println("Counter: " + counter);
+        while (adder != counter){
+            int keys = (int)aliens.keySet().toArray()[adder];
+            System.out.println("Key = "+ keys);
+            all.add(keys);
+            adder ++;
+        }
         
         return all;
     }
@@ -28,24 +43,16 @@ public class StudentSolver{
 
     public static void main(String args[]){
 
-        Scanner input = new Scanner(System.in);
         HashMap<Integer,Integer> aliens = new HashMap<Integer,Integer>();
-        System.out.println("Please enter a number: ");
-        int counter = input.nextInt(); //gets the number of aliens you want
-        int hold = counter;
-        System.out.println("Please enter a Levels: ");
-        int level = input.nextInt();//gets the number of levels
-        while(counter > 0 ){
-            aliens.put(counter,level);
-            counter--;
-            if(counter % 5 == 0){
-                level--;
-            }
-        }
-        System.out.println(aliens + " " + aliens.size());
+        aliens.put(1, 3);
+        aliens.put(2, 1);
+        aliens.put(3, 2);
+        aliens.put(4, 3);
+        aliens.put(5, 3);
+        aliens.put(6, 2);
         HashSet<Integer> totalAliens = new HashSet<Integer>();
         totalAliens = solve(aliens);
-        System.out.println(totalAliens);
+        System.out.println("Aliens: " + totalAliens);
         
 
         
--- Test/Java/test.java
+++ Test/Java/test.java
@@ -1,10 +1,33 @@
-import java.lang.Math;
+import java.util.*;
 
 public class test {
 
     public static void main(String args[]){
-        int first = ((int)Math.pow(2,3)) * ((int)Math.pow(2,4) + 4);
-        System.out.println(first);
+        HashMap<Integer,Integer> aliens = new HashMap<Integer,Integer>();
+        aliens.put(4, 2);
+        aliens.put(5, 1);
+        aliens.put(6, 2);
+        Iterator traverse = aliens.entrySet().iterator();
+        while (traverse.hasNext()) { 
+            Map.Entry mapElement = (Map.Entry)traverse.next(); 
+            int marks = ((int)mapElement.getValue()); 
+            System.out.println(mapElement.getKey() + " : " + marks); 
+        } 
+        System.out.println(aliens.entrySet());
+        System.out.println(aliens.get(aliens.keySet()));
+        sum += (Math.pow(2,-aliens.get(increase)));
+    }
+}
+
+if( n > 0 ){
+    for(int i = 0; i < n; i++){
+        counter++;
+        sum += (Math.pow(2,-aliens.get(increase)));
+        System.out.println("Sum = " +sum);
         
+        if(sum >= magicNum ){
+            break; 
+        }
+        increase++;
     }
 }
